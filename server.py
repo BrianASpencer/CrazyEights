a8e774bd27224f8e4097bfe9f0b25dc946c6c5d5
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2020-02-06 16:29:58 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,52 @@
-#!/usr/bin/env python
+# Brian Spencer
+# CSC 460
+# 2/6/2020
 
-import socket
-#server
- 
-host_name = socket.gethostname() 
-host_ip = socket.gethostbyname(host_name)
+from socket import *
+import threading
 
-TCP_IP = host_ip
+# variable to tell server to exit
+keepRunning = True
 
-TCP_PORT = 5005
+# define a function to handle a single client
+def client(clientSocket):
+	while True:
+		# get a message from the client
+		move = clientSocket.recv(1024).upper()
+		
+		# empty - Assume the client has disconnected
+		if len(move) == 0:
+			return
+		if move == "Q":
+			print('A player chose to quit. Server shutting down.')
+			keepRunning = False
+			return
+	
+		# update
+		clientSocket.send(theirHand)
+		clientSocket.send(currValue)
+		clientSocket.send(currSuit)
+	# end function client()
 
-BUFFER_SIZE = 1024
+# function to handle the main server socket accept loop
+def listenLoop(svrPort):
+	# create the socket
+	serverSocket = socket( AF_INET, SOCK_STREAM )
+	serverSocket.bind( ('',serverPort) )
+	#number of cleints
+	serverSocket.listen(2)
 
-MESSAGE = "Hello, World!"
+	print('The server is ready to receive')
+	while keepRunning:
+		# get a connection from a client
+		connectionSocket, addr = serverSocket.accept()
+		
+		# start up a thread for that client
+		cl = threading.Thread( target=client, args=(connectionSocket,), daemon=True )
+		cl.start()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(2)
+svrPort = int(input('Enter port number for the server: ' ))
 
-print(TCP_IP)
-
-while True:
-    conn, addr = s.accept()
-    data = conn.recv(BUFFER_SIZE)
-    if not data: 
-        break
-    print("received data: ", data)
-    conn.send(data)  # echo
-  
-conn.close()
+#start a thread for each client that connects
+sthread = threading.Thread( target=listenLoop, args=(svrPort,), daemon=True )
+sthread.start()
